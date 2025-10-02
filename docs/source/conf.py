19cc15660120b4527dc0e723197506b675c70bb1
The-Chuong TRINH <90518534+trinhthechuong@users.noreply.github.com>
2025-10-02 23:09:12 +0200

    Update project information in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SynOmics'
+copyright = '2025, Trinh'
+author = 'The-Chuong Trinh, Guido Ugguzoni, Jean-Baptiste Woillard, Christophe Battail'
 
 release = '0.1'
 version = '0.1.0'
